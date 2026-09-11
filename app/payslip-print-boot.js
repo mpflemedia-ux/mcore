d4a211bfc3fd7e537781c223382fcc4372b6bb58
Mike Paul <mpflemedia@gmail.com>
2026-09-11 22:09:07 +0800

    revert: original stacked payslip layout

--- app/payslip-print-boot.js
+++ app/payslip-print-boot.js
@@ -1,18 +1,4 @@
-(function(){
-  var href='./payslip-print.css?v=2';
-  var el=document.querySelector('link[data-ps-print]');
-  if(!el){
-    el=document.createElement('link');
-    el.rel='stylesheet';
-    el.setAttribute('data-ps-print','1');
-    document.head.appendChild(el);
-  }
-  el.href=href;
-  function apply(){
-    if(!document.querySelector('.ps-stmt')) return;
-    if(typeof _pdocSetPageOrientation==='function') _pdocSetPageOrientation('landscape', 8);
-  }
-  apply();
-  setInterval(apply, 800);
-  window.addEventListener('beforeprint', apply);
+(function () {
+  var el = document.querySelector('link[data-ps-print]');
+  if (el) el.remove();
 })();
