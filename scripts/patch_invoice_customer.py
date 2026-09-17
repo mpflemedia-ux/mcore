cbde316df498e4fd442065c2813d7976062459f6
Mike Paul <mpflemedia@gmail.com>
2026-09-18 04:17:54 +0800

    fix: hard dark colors on probation dialog

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -32,7 +32,7 @@ SCRIPTS = [
     ('att-tracker-today.js', '1'),
     ('probation-dash.js', '3'),
     ('hr-list-nickname.js', '1'),
-    ('probation-dialog-contrast.js', '1'),
+    ('probation-dialog-contrast.js', '2'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
