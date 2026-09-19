53f84938d1fb17e4b476b67dcb8ec59f98894464
Mike Paul <mpflemedia@gmail.com>
2026-09-20 02:47:04 +0800

    chore: disable old route overlays, classifier is source

--- app/docs-ssm-legal.js
+++ app/docs-ssm-legal.js
@@ -1,13 +1,2 @@
-(function () {
-  function fix() {
-    var folder = document.getElementById('docs-folder');
-    var box = document.getElementById('docs-result');
-    if (!box || !folder) return;
-    var txt = box.textContent || '';
-    if (!/SSM|Borang D/i.test(txt)) return;
-    if (/01_Contracts/.test(folder.value)) {
-      folder.value = folder.value.replace(/01_Contracts & Agreements/g, '08_Legal');
-    }
-  }
-  setInterval(fix, 600);
-})();
+/* superseded by docs-classify.js */
+(function () {})();
