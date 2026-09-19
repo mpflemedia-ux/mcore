fedac7a748d49d32e51b87b502bdb65059559c9b
Mike Paul <mpflemedia@gmail.com>
2026-09-19 21:57:40 +0800

    fix: persist Documents as last page on refresh

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -38,7 +38,7 @@ SCRIPTS = [
     ('leave-slip.js', '4'),
     ('leave-email-click.js', '1'),
     ('outstation-amend.js', '1'),
-    ('doc-router-admin.js', '7'),
+    ('doc-router-admin.js', '8'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
