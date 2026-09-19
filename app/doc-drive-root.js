b4a5fa7c0df5a6a4f89fc4f738464459a3a21d31
Mike Paul <mpflemedia@gmail.com>
2026-09-19 22:47:04 +0800

    fix: default Documents OAuth client id

--- app/doc-drive-root.js
+++ app/doc-drive-root.js
@@ -1,5 +1,8 @@
 try {
+  var ROOT = '1XLo0KDErqPiGDXXiuwzNa9nW7TF0Kn74';
+  var CID = '490414473408-0gb8sv4d1s51rvorepp7bna1j7igenj7.apps.googleusercontent.com';
   if (!localStorage.getItem('mcore_docs_drive_root')) {
-    localStorage.setItem('mcore_docs_drive_root', '1XLo0KDErqPiGDXXiuwzNa9nW7TF0Kn74');
+    localStorage.setItem('mcore_docs_drive_root', ROOT);
   }
+  localStorage.setItem('mcore_docs_drive_client', CID);
 } catch (e) {}
