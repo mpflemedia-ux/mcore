36a63248f9d14ff3a34db713f1579de31345e751
Mike Paul <mpflemedia@gmail.com>
2026-09-26 02:09:24 +0800

    fix: admin team table columns fit content

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -62,6 +62,7 @@ SCRIPTS = [
     ('support-tenant.js', '2'),
     ('plan-code-fix.js', '1'),
     ('admin-content-mobile.js', '2'),
+    ('admin-team-table.js', '1'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
