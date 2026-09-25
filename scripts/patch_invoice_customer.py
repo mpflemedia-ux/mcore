8d436a20d7eb580d5faf9788d47852293d50ce90
Mike Paul <mpflemedia@gmail.com>
2026-09-26 00:32:42 +0800

    fix: do not treat Pro as Free when plan cache empty

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -60,6 +60,7 @@ SCRIPTS = [
     ('acc-full-tabs.js', '1'),
     ('dash-insight-nickname.js', '1'),
     ('support-tenant.js', '2'),
+    ('plan-code-fix.js', '1'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
