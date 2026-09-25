2ff7b97f5abc3471fa5fe83f5c4c4b9736df7b27
Mike Paul <mpflemedia@gmail.com>
2026-09-26 04:13:51 +0800

    fix: bank recon columns one line

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -71,7 +71,7 @@ SCRIPTS = [
     ('sl-items-fit.js', '1'),
     ('prod-table-fit.js', '1'),
     ('do-table-fit.js', '1'),
-    ('list-table-fit.js', '1'),
+    ('list-table-fit.js', '2'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
