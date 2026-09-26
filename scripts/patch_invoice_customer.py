cac88d9df8256eb18590328bca3d92f112b2ebe1
Mike Paul <mpflemedia@gmail.com>
2026-09-26 18:44:16 +0800

    fix: salary disbursement save whitelist and live totals

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -77,7 +77,7 @@ SCRIPTS = [
     ('jnl-print.js', '2'),
     ('jnl-search.js', '1'),
     ('coa-seed-advanced.js', '2'),
-    ('sd-employer-edit.js', '2'),
+    ('sd-employer-edit.js', '3'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
