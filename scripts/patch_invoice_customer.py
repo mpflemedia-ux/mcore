5e2bfb17e7016165f827762532173f898a21f53a
Mike Paul <mpflemedia@gmail.com>
2026-09-26 14:11:23 +0800

    feat: amendable employer EPF SOCSO EIS on salary disbursement

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -77,6 +77,7 @@ SCRIPTS = [
     ('jnl-print.js', '2'),
     ('jnl-search.js', '1'),
     ('coa-seed-advanced.js', '2'),
+    ('sd-employer-edit.js', '1'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
