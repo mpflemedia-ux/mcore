d46ed44ed7053f68e09fcdd4553713d8d7d321e4
Mike Paul <mpflemedia@gmail.com>
2026-09-26 23:23:40 +0800

    fix: statutory label/input alignment

--- scripts/patch_invoice_customer.py
+++ scripts/patch_invoice_customer.py
@@ -79,7 +79,7 @@ SCRIPTS = [
     ('coa-seed-advanced.js', '2'),
     ('sd-employer-edit.js', '6'),
     ('sd-print-fit.js', '1'),
-    ('sd-table-fit.js', '5'),
+    ('sd-table-fit.js', '6'),
 ]
 for name, ver in SCRIPTS:
     tag = '<script src="./'+name+'?v='+ver+'"></script>'
